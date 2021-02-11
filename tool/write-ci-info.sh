b747853b5562c07fe2b0f361f08c435e964710aa
Alexander Thomas <athom@google.com>
2021-02-11 12:02:09 +0100

    Fix typo in write-ci-info.sh

--- tool/write-ci-info.sh
+++ tool/write-ci-info.sh
@@ -6,7 +6,7 @@ echo "# WARNING: the values in this file get regenerated at build time.
 build-id: ${GITHUB_RUN_ID:-$TRAVIS_BUILD_ID}
 build-number: ${GITHUB_RUN_NUMBER:-$TRAVIS_BUILD_NUMBER}
 build-time: $(date)
-commit-sha: ${$GITHUB_SHA:-$TRAVIS_COMMIT}
+commit-sha: ${GITHUB_SHA:-$TRAVIS_COMMIT}
 job-id: ${GITHUB_ACTION:-$TRAVIS_JOB_ID}
 repo-slug: ${GITHUB_REPOSITORY:-$TRAVIS_REPO_SLUG}" > $FILE
 
