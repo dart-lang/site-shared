3117e4706a6d8099b3411b7ee4ee9c6dc2d1c2a9
Alexander Thomas <athom@google.com>
2021-02-11 12:23:14 +0100

    Use GITHUB_RUN_ID as job-id
    GitHub Actions doesn't expose its job id in the environment.

--- tool/write-ci-info.sh
+++ tool/write-ci-info.sh
@@ -7,7 +7,7 @@ build-id: ${GITHUB_RUN_ID:-$TRAVIS_BUILD_ID}
 build-number: ${GITHUB_RUN_NUMBER:-$TRAVIS_BUILD_NUMBER}
 build-time: $(date)
 commit-sha: ${GITHUB_SHA:-$TRAVIS_COMMIT}
-job-id: ${GITHUB_ACTION:-$TRAVIS_JOB_ID}
+job-id: ${GITHUB_RUN_ID:-$TRAVIS_JOB_ID}
 repo-slug: ${GITHUB_REPOSITORY:-$TRAVIS_REPO_SLUG}" > $FILE
 
 if [[ "$1" == '-v' ]]; then
