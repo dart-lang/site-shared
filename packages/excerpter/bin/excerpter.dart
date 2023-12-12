af780751ab1a3296a49204b03ad2eba73ccfb803
Parker Lougheed <parlough@gmail.com>
2023-12-11 22:30:44 -0600

    Add missing license

--- packages/excerpter/bin/excerpter.dart
+++ packages/excerpter/bin/excerpter.dart
@@ -1,3 +1,6 @@
+// Copyright (c) 2023. All rights reserved. Use of this source code
+// is governed by a MIT-style license that can be found in the LICENSE file.
+
 import 'dart:io' as io;
 
 import 'package:args/args.dart';
